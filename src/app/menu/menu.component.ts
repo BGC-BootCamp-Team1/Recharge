46113151ed316eb9ac6b987927ef9105e1506bca
Anyan Zhao <AZhao4@slb.com>
2024-11-05 20:18:40 +0800

    feat: apply different promotion when click

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-import { RechargeFormComponent } from "../recharge-form/recharge-form.component";
+import { RechargeFormComponent } from '../recharge-form/recharge-form.component';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
   imports: [RechargeFormComponent],
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.css'
+  styleUrls: ['./menu.component.css']
 })
-export class MenuComponent {
-
-}
+export class MenuComponent { }
--- src/app/recharge-form/recharge-form.component.ts
+++ src/app/recharge-form/recharge-form.component.ts
@@ -1,23 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-
-@Component({
-  selector: 'app-recharge-form',
-  standalone: true,
-  imports: [ReactiveFormsModule, CommonModule],
-  templateUrl: './recharge-form.component.html',
-  styleUrl: './recharge-form.component.css'
-})
 
 export interface Promotion {
-  id: number;
+  id: number,
   description: string;
   applyPromotion(amount: number): number;
 }
 
-export class RechargeFormComponent {
-  rechargeForm: FormGroup;  
+@Component({
+  selector: 'app-recharge-form',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './recharge-form.component.html',
+  styleUrls: ['./recharge-form.component.css']
+})
+export class RechargeFormComponent implements OnInit {
+  rechargeForm: FormGroup;
   promotions: Promotion[] = [
     {
       id: 1,
@@ -36,42 +35,45 @@ export class RechargeFormComponent {
     }
   ];
 
-
   selectedPromotion: Promotion | undefined = undefined;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder) { 
     this.rechargeForm = this.fb.group({
       phone: ['', [Validators.required, Validators.pattern(/^1\d{10}$/)]],
-      discount: [''],
+      discount: ['', Validators.required],
       amount: ['', [Validators.required, Validators.min(10), Validators.max(3000)]]
-    });}
+    });
+
+  }
+
+  ngOnInit() {
+  }
+
+  setAmount(amount: number) {
+    this.rechargeForm?.patchValue({ amount });
+  }
+
+  calculatePaymentAmount() {
+    const amount = this.rechargeForm?.get('amount')?.value;
+    if (!this.selectedPromotion)
+      return 0;
+    return this.selectedPromotion.applyPromotion(amount);
+  }
 
   onSubmit() {
-    if (this.rechargeForm.valid) {
-      console.log(this.rechargeForm.value);
+    if (this.rechargeForm?.valid) {
+      console.log('Form Submitted', this.rechargeForm.value);
     } else {
       console.log('Form is invalid');
     }
   }
 
-  setAmount(amount: number) {
-    this.rechargeForm.get('amount')?.setValue(amount);
-  }
-
-  calculatePaymentAmount(): number {
-    const amount = this.rechargeForm.get('amount')?.value;
-    if (!this.selectedPromotion)
-    {
-      return 0;
-    }
-    return this.selectedPromotion.applyPromotion(amount);
-  }
-  
+  // 只允许输入数字
   onPhoneInput(event: any) {
     const input = event.target.value;
-    this.rechargeForm.controls['phone'].setValue(input.replace(/[^0-9]/g, ''));
+    this.rechargeForm?.controls['phone'].setValue(input.replace(/[^0-9]/g, ''));
   }
-  
+
   onPromotionChange(event: any) {
     const selectedId = parseInt(event.target.value, 10);
     this.selectedPromotion = this.promotions.find(promotion => promotion.id === selectedId);
