701e9a978b237f943aeaaa1ff5727dcdc5f81674
Sheng Liu <SLiu40@slb.com>
2024-11-05 19:17:35 +0800

    feat:add phone number component

--- src/app/component/recharge-form/recharge-form.component.ts
+++ src/app/component/recharge-form/recharge-form.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import {PhoneNumberComponent} from "../phone-number/phone-number.component";
 
 @Component({
   selector: 'app-recharge-form',
   standalone: true,
-  imports: [],
+  imports: [
+    PhoneNumberComponent
+  ],
   templateUrl: './recharge-form.component.html',
   styleUrl: './recharge-form.component.css'
 })
 export class RechargeFormComponent {
+  selectedDiscount: string = 'Mobile Store Recharge 9.95% Discount';
 
+  updateDiscountTag(event: Event) {
+    const selectElement = event.target as HTMLSelectElement;
+    this.selectedDiscount = selectElement.options[selectElement.selectedIndex].text;
+  }
 }
