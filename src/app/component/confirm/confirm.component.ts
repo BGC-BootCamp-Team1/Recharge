16fb7ab3ae261c20ee0bc066db0ab368980205eb
Sheng Liu <SLiu40@slb.com>
2024-11-05 21:44:56 +0800

    feat:update component

--- src/app/component/confirm/confirm.component.ts
+++ src/app/component/confirm/confirm.component.ts
@@ -2,14 +2,19 @@ import {Component, OnInit} from '@angular/core';
 import {MenuComponent} from "../menu/menu.component";
 import {RechargeFormComponent} from "../recharge-form/recharge-form.component";
 import {ActivatedRoute, Router} from "@angular/router";
+import {MatCard, MatCardActions, MatCardContent, MatCardHeader} from "@angular/material/card";
 
 @Component({
   selector: 'app-confirm',
   standalone: true,
-    imports: [
-        MenuComponent,
-        RechargeFormComponent
-    ],
+  imports: [
+    MenuComponent,
+    RechargeFormComponent,
+    MatCard,
+    MatCardHeader,
+    MatCardContent,
+    MatCardActions
+  ],
   templateUrl: './confirm.component.html',
   styleUrl: './confirm.component.css'
 })
--- src/app/component/menu/menu.component.ts
+++ src/app/component/menu/menu.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
+import {NgClass} from "@angular/common";
+import {NavigationEnd, Router} from "@angular/router";
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
+  activeStep: number = 1;
 
+  constructor(private router: Router) {
+
+    this.router.events.subscribe((val) => {
+      if (val instanceof NavigationEnd) {
+        this.updateActiveStep(val.url);
+      }
+    });
+  }
+
+  ngOnInit() {
+    // 直接调用 updateActiveStep 并传递当前 URL
+    this.updateActiveStep(this.router.url);
+
+  }
+
+
+  updateActiveStep(url: string) {
+    const cleanUrl = url.split('?')[0]; // 去掉查询参数
+    console.log('Clean URL:', cleanUrl); // 添加日志
+    if (cleanUrl === '/') {
+      this.activeStep = 1;
+    } else if (cleanUrl.startsWith('/confirm')) {
+      this.activeStep = 2;
+    } else {
+      this.activeStep = 3;
+    }
+  }
 }
