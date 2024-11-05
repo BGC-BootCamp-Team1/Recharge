e707c3a247254c410ad7abc79039a7b050990195
Sheng Liu <SLiu40@slb.com>
2024-11-05 18:43:25 +0800

    feat:add rechargeForm component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MenuComponent} from "./component/menu/menu.component";
+import {RechargeFormComponent} from "./component/recharge-form/recharge-form.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MenuComponent],
+  imports: [RouterOutlet, MenuComponent, RechargeFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
